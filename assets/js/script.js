3d9689a98d0770ae7a62a8b30a08cc8bcf7f15a5
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-07 00:07:56 -0400

    Page is laid out in html using bootstrap css and provided css classes in develop folder. Text area is working, though not functional per project guidlines. date is now shown at the top of the page

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// declare El and ID variables
+var currentDay = $("#currentDay")
+var hourEl = $(".hour");
+// create moments
+$("#currentDay").text(moment().format("dddd, MMM Do"));
+// create functions
+
+// create local storage
+
+// create functions
+
+// add event listeners
+
+// console logs for debug
+console.log(hourEl.length);
